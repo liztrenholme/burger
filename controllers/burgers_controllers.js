9cab19a43bea66a55db24e43a8e848e0599df054
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-05-04 20:25:49 -0400

    router is working, even though there is a big fat error for something else now

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,89 +1,56 @@
 var express = require("express");
+var router = express.Router();
 var burger = require("../models/burger.js");
 //var router = express.Router()
 // module.exports = function() {
 //     
 
-//     // redirect to '/index'
-//     router.get('/', function(req, res) {
-//         res.redirect('/index');
-//     });
+// module.exports = function() {
+//var app = express;
 
-
-//     // show all burgers on page
-//     router.get('/index', function(req, res) {
-//         burger.selectAll(function(data) {
-//             var hbsObject = { 
-//             	burgers: data 
-//             };
-//             console.log(hbsObject);
-//             res.render('index', hbsObject);
-//         });
-//     });
-
-
-//     // make a new burger
-//     router.post('/burger/create', function(req, res) {
-//         burger.insertOne(req.body.burger, function() {
-//             res.redirect('/index');
-//         });
-//     });
-
-
-//     // eat a burger
-//     router.post('/burger/eat/:id', function(req, res) {
-//         burger.updateOne(req.params.id, function() {
-//             res.redirect('/index');
-//         });
-//     });
-//     return router;
-// };
-module.exports = function() {
-	var app = express;
-	var router = express.Router();
-	//app.use(router);
+//app.use(router);
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burger: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+    burger.all(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
 });
 
-router.post("/", function(req, res) {
-  burger.create([
-    "burger", "devoured"
-  ], [
-    req.body.burger, req.body.devoured
-  ], function(data) {
-    res.redirect("/");
-  });
+router.post("/api/burgers/", function(req, res) {
+    burger.create([
+        "burger", "devoured"
+    ], [
+        req.body.burger, req.body.devoured
+    ], function(result) {
+        res.redirect("/");
+    });
 });
 
 
-router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put("api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+    console.log("condition", condition);
 
-  burger.update({
-    "devoured": req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
-  });
+    burger.update({
+        "devoured": req.body.devoured
+    }, condition, function(result) {
+        res.redirect("/");
+    });
 });
 
-router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function() {
-    res.redirect("/");
-  });
+    burger.delete(condition, function(result) {
+        res.redirect("/");
+    });
 });
-return router;
-};
+// return router;
+// };
 
 // Export routes for server.js to use.
-//module.exports = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
-
-// ***********************
-//var routeSource = require("./controllers/burgers_controllers.js");
-// require source of routes!!!!
-// var mysql = require("mysql");
-//var connection = require("./config/connection.js")
-
-var app = express();
-
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 7000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// *******************
-//app.use(router);
-// *******************
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-require("./controllers/burgers_controllers.js")(app);
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
